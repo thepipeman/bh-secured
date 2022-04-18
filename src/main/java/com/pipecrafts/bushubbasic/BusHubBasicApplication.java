4e6ef0192d8fedd7b00fac9976890d2129ab95ca
Aaron Rasing <maaronrasing@gmail.com>
2022-04-18 21:58:11 +0800

    initial commit

---
+++ src/main/java/com/pipecrafts/bushubbasic/BusHubBasicApplication.java
@@ -0,0 +1,13 @@
+package com.pipecrafts.bushubbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusHubBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusHubBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pipecrafts/bushubbasic/BusHubBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipecrafts.bushubbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusHubBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
