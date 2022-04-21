5937e82f5a1bcc8752fb74443d69f96d3233a596
Aaron Rasing <maaronrasing@gmail.com>
2022-04-21 22:56:45 +0800

    simple configuration override

--- src/main/java/com/pipecrafts/bushubbasic/common/config/security/AppSecurityConfig.java
+++ src/main/java/com/pipecrafts/bushubbasic/common/config/security/AppSecurityConfig.java
@@ -2,6 +2,8 @@ package com.pipecrafts.bushubbasic.common.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,7 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-public class AppSecurityConfig {
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Bean
   public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
@@ -30,4 +32,18 @@ public class AppSecurityConfig {
     return new BCryptPasswordEncoder();
   }
 
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+      .httpBasic();
+    // basic / default spring boot config
+    http.authorizeRequests()
+      .anyRequest().authenticated();
+
+    // authorizes all request
+//    http.authorizeRequests()
+//      .anyRequest().permitAll();
+
+
+  }
 }
